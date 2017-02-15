72a9b780964f8829a6e7d48a089eca4587302f37
Fase <andrfase@amazon.com>
2017-02-15 11:31:58 -0800

    WIP - adjusting CF template + generating DBR file path

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# INPUT Parameters
+#
+# $1 - S3 bucket to use
+# $2 - AWS Account ID
 
 # INSTALL PRE-REQS
 sudo mkdir /opt/drill
 sudo mkdir /opt/drill/log
 sudo chmod 777 /opt/drill/log
 sudo curl -s "http://download.nextag.com/apache/drill/drill-1.9.0/apache-drill-1.9.0.tar.gz" | sudo tar xz --strip=1 -C /opt/drill
-sudo yum install -y java
+sudo yum install -y java-1.8.0-openjdk
 sudo yum install -y python35
 sudo yum install -y aws-cli
 
@@ -14,4 +18,6 @@ sudo yum install -y aws-cli
 export PATH=/opt/drill/bin:$PATH
 
 # Start Athena Proxy
-PORT=10000 java -cp ./athenaproxy/athenaproxy.jar com.getredash.awsathena_proxy.API .
+PORT=10000 java -cp ./athenaproxy/athenaproxy.jar com.getredash.awsathena_proxy.API . &
+
+DBRFILE="s3://${1}/${2}-aws-billing-detailed-line-items-with-resources-and-tags-$(date +%Y%m).csv.zip"
